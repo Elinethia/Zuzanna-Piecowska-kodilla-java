0a8473b401a84e5461fce9b0270177e5dddbea7d
Zuzanna Piecowska <zuzanna.piecowska@onet.pl>
2021-10-23 17:25:16 +0200

    7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
+++
@@ -1,9 +0,0 @@
-package com.kodilla.stream.beautifier;
-
-public class PoemBeautifier {
-
-     String beautify(String poem, PoemDecorator poemDecorator) {
-         return poemDecorator.decorate(poem);
-     }
-
-}
