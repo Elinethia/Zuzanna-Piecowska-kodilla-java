3f401ec49bcf7d6932cc95284a8e72af4ff46397
Zuzanna Piecowska <zuzanna.piecowska@onet.pl>
2021-12-13 22:05:48 +0100

    13.1

--- kodilla-good-patterns/src/main/java/com/kodilla/kodillagoodpatterns/challanges/MovieStoreApplication.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/kodillagoodpatterns/challanges/MovieStoreApplication.java
@@ -1,9 +1,5 @@
 package com.kodilla.kodillagoodpatterns.challanges;
 
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 public class MovieStoreApplication {
 
     public static void main(String[] args) {
